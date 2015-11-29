5c5b40f60e1ad1ee4de1e55d0b68b88b6567fe5d
ynandak <ynandak@users.noreply.github.com>
2015-11-28 23:55:56 -0500

    Update firefox.bat

--- build-vm/scripts/firefox.bat
+++ build-vm/scripts/firefox.bat
@@ -1,2 +1,3 @@
+if not exist "C:\Installer" mkdir "C:\Installer"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/SoftwareEngineeringToolDemos/ICSE-2011-FireDetective/raw/master/build-vm/files/Firefox.exe','C:\Installer\Firefox.exe');
 C:\Installer\Firefox.exe -ms
